d0d2a24223bdcd436e22767efe9108c79e74cba4
Christopher <chstr@stud.ntnu.no>
2020-01-16 12:38:58 +0100

    deleted useless files
    Signed-off-by: Christopher <chstr@stud.ntnu.no>

--- foo.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int meme(){
-    int crash = 1;
-    if(crash){
-        crash = 0;
-    }
-    return 0;
-}
--- testing.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int foo(){
-    if(1){
-        //do something
-    }
-    return 0;
-}
