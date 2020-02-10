98e5022a4ed78af2bb2891803b8c44f2461e202d
S. Anntid <Sanntid@Sanntid.std>
2020-02-10 14:16:13 +0100

    Add button IO functionality
    Signed-off-by: S. Anntid <Sanntid@Sanntid.std>

--- source/elevator_io.c
+++ source/elevator_io.c
@@ -1,21 +1,50 @@
 #include "elevator_io.h"
 
-void update_floor_lights(int* p_up, int* p_down) {
+int at_floor() {
+    for(int floor = MIN_FLOOR; floor <= MAX_FLOOR; floor++) {
+       if(hardware_read_floor_sensor(floor)) {
+           return floor;
+       }
+    }
 
+    return 0;
 }
 
-void update_cab_lights(int* p_cab) {
-
-}
-
-void update_floor_button_lights(int floor) {
-
-}
-
-void set_stop_light(int stop_light) {
-
-}
-
-void set_door_open_light(int door_open){
+// Naive approach; iterate through the floors, and set order_floor accordingly
+// Does not account for buttons already pressed
+void poll_floor_buttons(Order* queue, int* p_order_up, int* p_order_down) {
     
+    int order_floor = 0;
+  
+    for(int floor = MIN_FLOOR; floor <= MAX_FLOOR; floor++) {
+        int order_up = 0;
+        int order_down = 0;
+
+        if(!p_order_up[floor]){
+            order_up = hardware_read_order(floor, HARDWARE_ORDER_UP);
+            p_order_up[floor] = 1;
+        }
+     
+        if(!p_order_down[floor]){
+            order_down = hardware_read_order(floor, HARDWARE_ORDER_DOWN);
+            p_order_down[floor] = 1;
+        }
+
+        if(order_up || order_down) {
+            order_floor = floor;
+        }
+    }
+
+    if(order_floor != 0){ 
+        Order order = {.floor_at = order_floor};
+        queue->push_back(order);
+    }
+
+}
+
+//May want to change the in-argument to queue
+void update_cab_buttons(Order* p_current_order) {
+    for(int cab_button = MIN_FLOOR; cab_button <= MAX_FLOOR; cab_button++) {
+        p_current_order->floor_to[cab_button] = hardware_read_order(cab_button, HARDWARE_ORDER_INSIDE);
+    }
 }
--- source/elevator_io.h
+++ source/elevator_io.h
@@ -10,37 +10,32 @@
 #include "queue.h"
 
 
-// Mye overlap med utdelt kode!!!!! Mest sannsynlig å fjerne set-funksjoner, men å beholde update/get
-
 /**
  * @brief Find what floor the elevator is at
  * 
  * @return 0 if the elevator is between floors, and an int corresponding to the current floor
  * if the elevator is at a floor
  */
-int at_floor() {
-
-}
+int at_floor();
 
 
 /**
  * @brief Poll all floor buttons
  * 
- * @return An @c Order object with the first floor button that was detected to be pressed
- * as its @c floor_at value.
- * 
-*/
-Order poll_floor_buttons() {
+ * @param[out] queue                A pointer to the queue, to which an @c Order object will be added if a
+ * floor button is pressed
+ * @param[in][out] p_order_up       A pointer to an array containing the states of the up-buttons
+ * @param[in][out] p_order_down     A pointer to an array containing the states of the down-buttons
+ */
+void poll_floor_buttons(Order* queue, int* p_order_up, int* p_order_down) 
 
-}
 
 /**
+ * @brief Set the cab button requests for an @c Order
  * 
+ * @param[out] p_current_order  A pointer to the order to be updated
  */
-void update_cab_buttons(Order* current_order) {
-
-}
-
+void update_cab_buttons(Order* p_current_order);
 
 
 #endif
