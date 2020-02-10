7354f6d3fbc17b9ce192b05975a6c7cad00fbe0c
S. Anntid <Sanntid@Sanntid.std>
2020-02-10 14:52:45 +0100

    Update queue desc/func
    Signed-off-by: S. Anntid <Sanntid@Sanntid.std>

--- source/queue.h
+++ source/queue.h
@@ -10,7 +10,7 @@
 typedef struct{
     int floor_at;               // Floor at which the floor button was pressed
     int floor_to[MAX_FLOOR];    // The cab orders
-    int dir;                    // 1 = up, -1 = down
+    int dir;
 } Order;
 
 /**
@@ -24,19 +24,23 @@ void update_queue(Order* p_queue);
  * @brief Set the cab orders for a given @c Order
  * 
  * @param[in][out] p_current_order  A pointer to the @c Order to which the cab orders will be set
+ * 
+ * Upon arrival at the target floor for an @c Order , we need to set the cab orders for this current order.
 */
 void set_cab_orders(Order* p_current_order);
 
+
 /**
- * @brief Clear a given @c Order of a cab order  a given floor
+ * @brief Clear a given @c Order of a cab order for a given floor
  * 
+ * @param[out] p_queue  A pointer to the queue
+ * @param[in]  floor    The floor to be used for clearing the cab orders in the 
+ * 
+ * Any given order can have MAX_FLOOR amount of cab orders. Upon arrival at a floor,
+ * all cab orders for the queue shall be considered handled; thereby clearing the order.
 */
-void clear_cab_orders(Order* p_current_order, int current_floor);
+void clear_cab_orders(Order* p_queue, int floor);
 
-
-void add_floor_orders(int* p_up, int* p_down) {
-
-}
 
 
 #endif
