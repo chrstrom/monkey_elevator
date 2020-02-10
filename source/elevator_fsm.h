7295ed94c8d3aa4e13e7445002f7d2c7bfa07090
Øystein Solbø <oyssolbo@stud.ntnu.no>
2020-02-10 15:35:03 +0100

    FSM-changes as 10.02.20
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- source/elevator_fsm.h
+++ source/elevator_fsm.h
@@ -8,22 +8,21 @@
 #define CHECK_OBSTRUCTION 1
 #define START_DOOR_TIMER 2
 
+#include "includes.h"
+#include "queue.h"
+#include "timer.h"
+#include "elevator_io.h"
 
-/**
- * In idle state:
- * If queue empty, do nothing
- * 
- */
 typedef enum{
     STATE_IDLE,
     STATE_MOVING_UP,
-    STATE_MOVING_DOWN,
+    STATE_MOVING_DOWN
     //STATE_PREP_MOVE,
     //STATE_SERVE_ORDER,
     //STATE_ERROR
 } elevator_state_t;
 
-int update_state(elevator_state_t* current_state);
+int update_state(elevator_state_t* p_current_state, time_t* p_ref_time, Order* p_current_order);
 
 // static int state_transition_table[][];
 
