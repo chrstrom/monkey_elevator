93c0f2d7df9af9ef347fa5e342754ad20daf8b31
S. Anntid <Sanntid@Sanntid.std>
2020-02-11 13:18:18 +0100

    Change timer from ptr to value
    Signed-off-by: aleksava <aleksava@stud.ntnu.no>

--- source/timer.c
+++ source/timer.c
@@ -1,11 +1,11 @@
 #include "timer.h"
 
 void start_timer(time_t* p_timer){
-    *(p_timer) = time(0);
+    *(p_timer) = time(NULL);
 }
 
 int check_timer(time_t* p_timer){
-    unsigned int time_diff = (unsigned int)(time(0) - *(p_timer));
+    unsigned int time_diff = (unsigned int)(time(NULL) - *(p_timer));
     //Might be a bug here! Comparing unsigned int and int
     if(time_diff >= NORMAL_WAIT_TIME){
         return 1;
