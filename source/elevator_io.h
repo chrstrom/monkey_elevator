cc7e4ec21c3f458801d2bc8ed3d0d2e5b7fb0659
S.Anntid <Sanntid@Sanntid.stud>
2020-02-21 10:41:01 +0100

    Lowercase arrays
    Signed-off-by: S.Anntid <Sanntid@Sanntid.stud>

--- source/elevator_io.h
+++ source/elevator_io.h
@@ -30,23 +30,23 @@ void set_floor_indicator_light(int floor);
 /**
  * @brief Poll all floor buttons for input
  * 
- * @param[in, out] p_data    Pointer to @c elevator_data_t that contains the button arrays @c ORDERS_UP and @c ORDERS_DOW
+ * @param[in, out] p_data    Pointer to @c elevator_data_t that contains the button arrays @c orders_up and @c ORDERS_DOW
  * 
- * @warning This function operates on the assumption that @c ORDERS_UP and @c ORDERS_DOWN are 
+ * @warning This function operates on the assumption that @c orders_up and @c orders_down are 
  * set with 0's and 1's, respectively for "button not clicked" and "button clicked"
  * 
  * @warning This function also uses @c set_floor_button_lights() to set the button lights
  * 
  * The function checks every external elevator button, from the first floor to the last floor.
  * Upon finding a button that is clicked, that has not already been clicked ( by checking
- * the @c ORDERS_UP and @c ORDERS_DOWN arrays), the corresponding value in the array is set to 1.
+ * the @c orders_up and @c orders_down arrays), the corresponding value in the array is set to 1.
  */
 void poll_floor_buttons(int* p_orders_up, int* p_orders_down);
 
 /**
- * @brief Set the floor button lights in accordance to the values in @c ORDERS_UP and @c ORDERS_DOWN .
+ * @brief Set the floor button lights in accordance to the values in @c orders_up and @c orders_down .
  * 
- * @param[in] p_data    A pointer to @c elevator_data_t that contains the @c ORDERS_UP and @c ORDERS_DOWN
+ * @param[in] p_data    A pointer to @c elevator_data_t that contains the @c orders_up and @c orders_down
  */
 void set_floor_button_lights(int* p_orders_up, int* p_orders_down);
 
@@ -54,7 +54,7 @@ void set_floor_button_lights(int* p_orders_up, int* p_orders_down);
 /**
  * @brief Poll all cab buttons for input
  * 
- * @param[in, out] p_data    A pointer to @c elevator_data_t that contains the @c ORDERS_CAB that
+ * @param[in, out] p_data    A pointer to @c elevator_data_t that contains the @c orders_cab that
  * will be set according to the input
  *
  * @warning This function uses @c set_cab_button_lights() to set the cab button lights
@@ -63,9 +63,9 @@ void set_floor_button_lights(int* p_orders_up, int* p_orders_down);
 void poll_cab_buttons(int* p_orders_cab);
 
 /**
- * @brief Set the cab button lights in accordance to the values in @c ORDERS_CAB
+ * @brief Set the cab button lights in accordance to the values in @c orders_cab
  * 
- * @param[in] p_data    A pointer to @c elevator_data_t that contains the @c ORDERS_CAB
+ * @param[in] p_data    A pointer to @c elevator_data_t that contains the @c orders_cab
  */
 void set_cab_button_lights(int* p_orders_cab);
 
