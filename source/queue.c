714cd366c7f4d743822eb4f7c06b896127d7951f
S.Anntid <Sanntid@Sanntid.stud>
2020-02-21 10:09:14 +0100

    Cleaned up queue lib
    Signed-off-by: S.Anntid <Sanntid@Sanntid.stud>

--- source/queue.c
+++ source/queue.c
@@ -2,10 +2,14 @@
 #include "elevator_io.h"
 #include <stdio.h>
 
+void set_single_order(int idx, int target_floor, HardwareOrder order_type) {
+    QUEUE[idx].target_floor = target_floor;
+    QUEUE[idx].order_type = order_type;
+}
+
 void init_queue() {
     for(int i = 0; i < QUEUE_SIZE; i++) {
-        QUEUE[i].target_floor = INVALID_ORDER;
-        QUEUE[i].order_type =  HARDWARE_ORDER_NOT_INIT;
+        set_single_order(i, INVALID_ORDER, HARDWARE_ORDER_NOT_INIT);
     }
 }
 
@@ -14,20 +18,17 @@ void update_queue(){
         return;
     }
 
-    // If there are any orders with target_floor = INVALID_ORDER, restructurate the queue
-    delete_holes_in_queue();
+    // If there are any orders with target_floor = INVALID_ORDER, restructure the queue
+    refactor_queue();
     
-    // QUEUE[0].target_floor should be INVALID_ORDER, if we had a target at the current_floor
-    // since we clear the orders before calling this function
+    // QUEUE[0].target_floor should be INVALID_ORDER, if we had a target at the current_floor since we clear the orders before calling this function
     if(QUEUE[0].target_floor == INVALID_ORDER){ 
         for (int ord = 0; ord < QUEUE_SIZE; ord++){
             if (ord < QUEUE_SIZE - 1){
-                QUEUE[ord].target_floor = QUEUE[ord + 1].target_floor;
-                QUEUE[ord].order_type = QUEUE[ord + 1].order_type;
+                set_single_order(ord, QUEUE[ord + 1].target_floor, QUEUE[ord + 1].order_type);
             }
             else if (ord == QUEUE_SIZE - 1){
-                QUEUE[ord].target_floor = INVALID_ORDER;
-                QUEUE[ord].order_type = HARDWARE_ORDER_NOT_INIT;
+                set_single_order(ord, INVALID_ORDER, HARDWARE_ORDER_NOT_INIT);
             }
         }
     }
@@ -40,15 +41,13 @@ void erase_queue(int* p_orders_up, int* p_orders_down, int* p_orders_cab){
 }
 
 
-void delete_holes_in_queue(){
-    for(int ord = 0; ord < QUEUE_SIZE; ord++){
-        if(QUEUE[ord].target_floor == INVALID_ORDER){
-            for(int inv = ord; inv < QUEUE_SIZE; inv++){
-                if(QUEUE[inv].target_floor != INVALID_ORDER){
-                    QUEUE[ord].target_floor = QUEUE[inv].target_floor;
-                    QUEUE[ord].order_type = QUEUE[inv].order_type;
-                    QUEUE[inv].target_floor = INVALID_ORDER;
-                    QUEUE[inv].order_type = HARDWARE_ORDER_NOT_INIT;
+void refactor_queue(){
+    for(int order = 0; order < QUEUE_SIZE; order++){
+        if(QUEUE[order].target_floor == INVALID_ORDER){
+            for(int hole = order; hole < QUEUE_SIZE; hole++){
+                if(QUEUE[hole].target_floor != INVALID_ORDER){
+                    set_single_order(order, QUEUE[hole].target_floor, QUEUE[hole].order_type);
+                    set_single_order(hole, INVALID_ORDER, HARDWARE_ORDER_NOT_INIT);
                     break;
                 }
             }
@@ -57,53 +56,23 @@ void delete_holes_in_queue(){
 }
 
 void push_back_queue(int target_floor, HardwareOrder order_type) {
-    //her burde det være en logikk som legger ting i rett rekkefølge
-    //altså dersom man har en ordre som skal ned i en høyere etasje, så kjører man først til den etasjen før man tar den lenger ned
-    //også motsatt: dersom man får ordre fra en etasje lengre ned som skal opp, så kjører man først til den etasjen
     for(int order = 0; order < QUEUE_SIZE; order++) {
         if(check_order_match(target_floor, order_type) == 1) {
-            return;
+            return; // Return if we have an order with the same parameters in the queue already
         }
     }
 
     for(int order = 0; order < QUEUE_SIZE; order++) {
         if(QUEUE[order].target_floor == INVALID_ORDER) {
-            QUEUE[order].target_floor = target_floor;
-            QUEUE[order].order_type = order_type;
+            set_single_order(order, target_floor, order_type);
             break;
         }
     }
-
-    //kall en sorteringsfunksjon som sorterer ordrene etter slik logikk som forespeilet øverst
 }
 
-// void push_front_queue(int floor, HardwareOrder order_type){
-//     //OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOoppdateeeeeeeeeeeeeeeeeeeer
-//     // for(int ord = QUEUE_SIZE - 1; ord > 0; ord--){
-//     //     QUEUE[ord].order_type = QUEUE[ord - 1].order_type;
-//     //     QUEUE[ord].target_floor = QUEUE[ord - 1].target_floor;
-//     // }
-//     sort_queue();
-//     for(int cab_order = 0; cab_order < QUEUE_SIZE; cab_order++){
-//         if(QUEUE[cab_order].order_type != HARDWARE_ORDER_INSIDE){
-//             Order temp = QUEUE[cab_order];
-//             QUEUE[cab_order].target_floor = INVALID_ORDER;
-//             QUEUE[cab_order].order_type = HARDWARE_ORDER_NOT_INIT;
-//             for(int floor_order = cab_order + 1; floor_order < QUEUE_SIZE; floor_order++){
-//                 Order temp2 = QUEUE[floor_order];
-//                 QUEUE[floor_order].target_floor = temp.target_floor;
-//                 QUEUE[floor_order].order_type = temp.order_type;
-//                 temp = temp2;
-//             }
-//             QUEUE[cab_order].target_floor = floor;
-//             QUEUE[cab_order].order_type =  order_type;
-//             break;
-//         }
-//     }
-// }
 
 int check_queue_empty() {
-    delete_holes_in_queue();
+    refactor_queue();
     return QUEUE[0].target_floor == INVALID_ORDER;
 }
 
@@ -119,6 +88,7 @@ int check_order_match(int current_floor, HardwareOrder order_type) {
             return 1;
         }
     }
+
     return 0;
 }
 
@@ -129,68 +99,9 @@ void clear_orders_at_floor(int* p_orders_cab, int* p_orders_up, int* p_orders_do
             p_orders_cab[order_floor] = 0;
             p_orders_up[order_floor] = 0;
             p_orders_down[order_floor] = 0;
-            QUEUE[order].target_floor = INVALID_ORDER;
-            QUEUE[order].order_type = HARDWARE_ORDER_NOT_INIT;
+            set_single_order(order, INVALID_ORDER, HARDWARE_ORDER_NOT_INIT);
         }
     }
 
-    //set_cab_button_lights(p_orders_cab);
-    //set_floor_button_lights(p_orders_up, p_orders_down);
     update_queue();
 }
-
-// void sort_queue(){
-//     //sorteringen skal sortere ordrene i heisen til først å gå etter ordre innenfra
-//     //så skal den prioritere ordre utenfra
-
-//     delete_holes_in_queue();
-
-//     Order* temp_queue = QUEUE;
-//      for(int floor = 0; floor < HARDWARE_NUMBER_OF_FLOORS; floor++) {
-//         QUEUE[floor].target_floor = INVALID_ORDER;
-//         QUEUE[floor].order_type = HARDWARE_ORDER_NOT_INIT;
-//     }
-
-//     for(int order = 0; order < QUEUE_SIZE; order++) {
-//         Order temp_order = temp_queue[order];
-//         if(temp_order.order_type == HARDWARE_ORDER_INSIDE) {
-//             push_front_queue(temp_order.target_floor, temp_order.order_type);
-//         }
-//         else {
-//             push_back_queue(temp_order.target_floor, temp_order.order_type);
-//         }
-//     }
-
-    // //sorting of the HARDWARE_ORDER_INSIDE/cab_buttons
-    // int sorted_queue_number = 0;
-    // for(int ord = 0; ord < QUEUE_SIZE; ord++){
-    //     if(QUEUE[ord].order_type == HARDWARE_ORDER_INSIDE){
-    //         if(QUEUE[sorted_queue_number].order_type != HARDWARE_ORDER_INSIDE){
-    //             int temp_floor = QUEUE[sorted_queue_number].target_floor;
-    //             HardwareOrder temp_order_type = QUEUE[sorted_queue_number].order_type;
-    //             QUEUE[sorted_queue_number].target_floor = QUEUE[ord].target_floor;
-    //             QUEUE[sorted_queue_number].order_type = QUEUE[ord].order_type;
-    //             QUEUE[ord].target_floor = temp_floor;
-    //             QUEUE[ord].order_type = temp_order_type;
-    //             sorted_queue_number++;
-    //         }
-    //     }
-    // }
-
-    // //skal sortere køen etter rekkefølgen som heisen skal prioritere av eksterne ordre
-
-    // //funksjonen vil endre target_floor til heisen
-
-    // int possible_target_floor = INVALID_ORDER;
-    // if (current_dir == HARDWARE_ORDER_UP){
-    //     for (int ord = 0; ord < QUEUE_SIZE; ord++){
-    //         if (QUEUE[ord].order_type == HARDWARE_ORDER_DOWN)
-    //         {
-    //             if(possible_target_floor == INVALID_ORDER){
-    //                 possible_target_floor = QUEUE[ord].target_floor;
-    //             }
-    //             possible_target_floor = QUEUE[ord].target_floor;
-    //         }
-    //     }
-    // }
-// }
--- source/queue.h
+++ source/queue.h
@@ -23,44 +23,38 @@ typedef struct{
 Order QUEUE[QUEUE_SIZE];                /**< The elevator's queue */
 
 
+void set_single_order(int idx, int target_floor, HardwareOrder order_type);
 /**
  * @brief Initialize the @c QUEUE with all invalid orders
  */
 void init_queue();
+
 
 /**
  * @brief Update a QUEUE by removing the first element.
  * 
  * @warning Will only work if the elevator is at a defined floor
  * 
- * The function will invoke delete_holes_in_queue(), such that the queue's valid orders
+ * The function will invoke refactor_queue(), such that the queue's valid orders
  * are in serial, and not split between invalid orders.
  */
 void update_queue();
+
 
 /**
  * @brief Empty the @c QUEUE by removing all elements
  */
 void erase_queue(int* p_orders_up, int* p_orders_down, int* p_orders_cab);
 
-// /**
-//  * @brief Reset a spesific order in the queue.
-//  *    
-//  * @param[in] floor      The floor we would like to reset an order to
-//  * @param[in] order_type The @c HardwareOrder we want to search for
-//  *
-//  * The function removes an @c Order from the @c QUEUE with given @p floor and @p order_type ,
-//  * and cleans up the queue by removing the hole it leaves.
-//  */
-
-// void erase_single_order(int floor, HardwareOrder order_type);
 
 /**
  * @brief Delete all occurencec of "holes" in the @c QUEUE
  * 
- * The function will left-shift orders until the @c QUEUE contains no holes
+ * The function will left-shift orders until the @c QUEUE contains no holes. A hole is defined as
+ * an empty order that splits apart an otherwise connected part of the queue.
  */
-void delete_holes_in_queue();
+void refactor_queue();
+
 
 /**
  * @brief Add orders to the @c QUEUE if an existing order with the same parameters is not in it
@@ -74,16 +68,6 @@ void delete_holes_in_queue();
  */
 void push_back_queue(int target_floor, HardwareOrder order_type);
 
-// /**
-//  * @brief Pushes a new @c Order to the front of the @c QUEUE
-//  * 
-//  * @param[in] floor         The floor that the @c Order came from
-//  * @param[in] order_type    The direction of the @c Order
-//  * 
-//  * The function right-shifts every element in the @c QUEUE and sets the first element to
-//  * @p floor and @p order_type 
-//  */
-// void push_front_queue(int floor, HardwareOrder order_type);
 
 /**
  * @brief Check if the @c QUEUE is empty
@@ -95,20 +79,6 @@ void push_back_queue(int target_floor, HardwareOrder order_type);
  */
 int check_queue_empty();
 
-/**
- * @brief Checks the @c QUEUE for an order with specified parameters
- *
- * @param[in] target_floor  The floor used in the @c QUEUE check
- * @param[in] order_type    The order_type used in the @c QUEUE check
- * 
- * @return 1 if the @c QUEUE contains an order matching the given parameters
- * and 0 if there are no matching orders in the @c QUEUE .
- * 
- * The function checks each element in the @c QUEUE for an
- * element matching both the @p target_floor and @p order_type and returns
- * a truth value representing the result of this check.
- */
-int check_queue_for_order(int target_floor, HardwareOrder order_type);
 
 /**
  * @brief Check if the QUEUE has a valid order to handle at a floor
@@ -123,6 +93,7 @@ int check_queue_for_order(int target_floor, HardwareOrder order_type);
  */
 int check_order_match(int target_floor, HardwareOrder order_type);
 
+
 /**
  * @brief Clear all orders in the @c QUEUE for the @p current_floor
  * 
@@ -130,21 +101,6 @@ int check_order_match(int target_floor, HardwareOrder order_type);
  * @param[in] current_floor The floor to delete the @c Order
  */
 void clear_orders_at_floor(int* p_orders_cab, int* p_orders_up, int* p_orders_down, int current_floor);
-
-
-// /**
-//  * @brief Set the cab orders according to cab buttons pressed
-//  * 
-//  * @param[in, out] p_data Pointer to @c elevator_data_t that contains the array for cab-orders 
-//  */
-// void set_cab_orders(int* p_order_array);
-
-// /**
-//  * @brief Clear a cab order for a given floor
-//  * @param[in, out] p_data        Pointer to @c elevator_data_t that will have it's cab-orders reset
-//  * @param[in] current_floor     The floor to be used for clearing the cab orders
-//  */
-// void clear_orders(int* p_array, int current_floor);
 
 
 #endif //QUEUE_H
