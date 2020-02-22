aafdc4e5c73cb0ab9676ab17d0fa1cbebbe2615a
Christopher <detoxte@gmail.com>
2020-02-22 22:16:44 +0100

    Potential way to avoid helper function for button lights
    Signed-off-by: Christopher <detoxte@gmail.com>

--- source/elevator_io.c
+++ source/elevator_io.c
@@ -2,6 +2,28 @@
 #include "globals.h"
 #include "queue.h"
 
+int elevator_init() {
+    //Turn off all button lights and clear all order light arrays (just in case)
+    for(int floor = 0; floor < HARDWARE_NUMBER_OF_FLOORS; floor++) {
+        hardware_command_order_light(floor, HARDWARE_ORDER_UP,     LIGHT_OFF);
+        hardware_command_order_light(floor, HARDWARE_ORDER_DOWN,   LIGHT_OFF);
+        hardware_command_order_light(floor, HARDWARE_ORDER_INSIDE, LIGHT_OFF);
+    }
+
+    //we assume the obstruction will never be active during setup!
+    hardware_command_stop_light(LIGHT_OFF);
+    hardware_command_door_open(DOOR_CLOSE); 
+
+    hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
+    while(get_current_floor() == BETWEEN_FLOORS) {}
+    hardware_command_movement(HARDWARE_MOVEMENT_STOP);
+
+    hardware_command_floor_indicator_on(get_current_floor());
+
+    init_queue();
+    return 0;
+}
+
 //Kanskje ønskelig å bruke disse funksjonene, slik at vi ikke trenger å kjenne til hardware i andre moduler!
 int check_stop_button(){
     return hardware_read_stop_signal();
@@ -18,11 +40,11 @@ void drive_elevator(HardwareMovement dir){
 }
 
 
-void set_cab_button_lights(int* p_orders_cab) {
-    for(int floor = MIN_FLOOR; floor < HARDWARE_NUMBER_OF_FLOORS; floor++) {
-        hardware_command_order_light(floor, HARDWARE_ORDER_INSIDE, p_orders_cab[floor]);
-    }
-}
+// void set_cab_button_lights(int* p_orders_cab) {
+//     for(int floor = MIN_FLOOR; floor < HARDWARE_NUMBER_OF_FLOORS; floor++) {
+//         hardware_command_order_light(floor, HARDWARE_ORDER_INSIDE, p_orders_cab[floor]);
+//     }
+// }
 
 
 void set_floor_indicator_light(int floor) {
@@ -48,33 +70,35 @@ void update_cab_buttons(int* p_orders_cab) {
             push_back_queue(floor, HARDWARE_ORDER_INSIDE);
             p_orders_cab[floor] = 1;
         }
+        hardware_command_order_light(floor, HARDWARE_ORDER_INSIDE, p_orders_cab[floor]);
     }
 
-    set_cab_button_lights(p_orders_cab);
+    //set_cab_button_lights(p_orders_cab);
 }
 
 
-void set_floor_button_lights(int* p_orders_up, int* p_orders_down) {
-    // The last floor does not have an up-button: Start at 0.
-    for(int floor_up = MIN_FLOOR; floor_up < HARDWARE_NUMBER_OF_FLOORS - 1; floor_up++) {
-        hardware_command_order_light(floor_up, HARDWARE_ORDER_UP, p_orders_up[floor_up]);
-    }
+// void set_floor_button_lights(int* p_orders_up, int* p_orders_down) {
+//     // The last floor does not have an up-button: Start at 0.
+//     for(int floor_up = MIN_FLOOR; floor_up < HARDWARE_NUMBER_OF_FLOORS - 1; floor_up++) {
+//         hardware_command_order_light(floor_up, HARDWARE_ORDER_UP, p_orders_up[floor_up]);
+//     }
 
-    // The first floor does not have a down-button: Start at 1.
-    for(int floor_down = MIN_FLOOR + 1; floor_down < HARDWARE_NUMBER_OF_FLOORS; floor_down++) {
-        hardware_command_order_light(floor_down, HARDWARE_ORDER_DOWN, p_orders_down[floor_down]);
-    }
-}
+//     // The first floor does not have a down-button: Start at 1.
+//     for(int floor_down = MIN_FLOOR + 1; floor_down < HARDWARE_NUMBER_OF_FLOORS; floor_down++) {
+//         hardware_command_order_light(floor_down, HARDWARE_ORDER_DOWN, p_orders_down[floor_down]);
+//     }
+// }
 
 
 
 void update_floor_buttons(int* p_orders_up, int* p_orders_down) {
-  
+    // The last floor does not have an up-button: Start at 0.
     for(int floor_up = MIN_FLOOR; floor_up < HARDWARE_NUMBER_OF_FLOORS - 1; floor_up++) {
         if(p_orders_up[floor_up] == 0 && hardware_read_order(floor_up, HARDWARE_ORDER_UP) == 1){
             push_back_queue(floor_up, HARDWARE_ORDER_UP);
             p_orders_up[floor_up] = 1;
         }
+        hardware_command_order_light(floor_up, HARDWARE_ORDER_UP, p_orders_up[floor_up]);
     }
 
     // The first floor does not have a down-button: Start at 1.
@@ -83,30 +107,8 @@ void update_floor_buttons(int* p_orders_up, int* p_orders_down) {
             push_back_queue(floor_down, HARDWARE_ORDER_DOWN);
             p_orders_down[floor_down] = 1;
         }
+        hardware_command_order_light(floor_down, HARDWARE_ORDER_DOWN, p_orders_down[floor_down]);
     }
     // After getting all button events, add orders to queue and set the button lights
-    set_floor_button_lights(p_orders_up, p_orders_down);
-}
-
-
-int init_elevator() {
-    //Turn off all button lights and clear all order light arrays (just in case)
-    for(int floor = 0; floor < HARDWARE_NUMBER_OF_FLOORS; floor++) {
-        hardware_command_order_light(floor, HARDWARE_ORDER_UP,     LIGHT_OFF);
-        hardware_command_order_light(floor, HARDWARE_ORDER_DOWN,   LIGHT_OFF);
-        hardware_command_order_light(floor, HARDWARE_ORDER_INSIDE, LIGHT_OFF);
-    }
-
-    //we assume the obstruction will never be active during setup!
-    hardware_command_stop_light(LIGHT_OFF);
-    hardware_command_door_open(DOOR_CLOSE); 
-
-    hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
-    while(get_current_floor() == BETWEEN_FLOORS) {}
-    hardware_command_movement(HARDWARE_MOVEMENT_STOP);
-
-    hardware_command_floor_indicator_on(get_current_floor());
-
-    init_queue();
-    return 0;
+    //set_floor_button_lights(p_orders_up, p_orders_down);
 }
--- source/elevator_io.h
+++ source/elevator_io.h
@@ -5,13 +5,23 @@
 #ifndef ELEVATOR_IO_H
 #define ELEVATOR_IO_H
 
-
 /**
- * @brief Set the cab button lights in accordance with the values in @p p_orders_cab
+ * @brief Initialize the elevator
  * 
- * @param[in, out] p_orders_cab      A pointer to an array of the cab-button states
+ * @return A true value on successful initialization, 0 if it failed.
+ * 
+ * Initiailize the elevator by setting all connected values to its default values, and, 
+ * if the elevator isn't already at a floor, drive the elevator down to the first valid floor.
  */
-void set_cab_button_lights(int* p_orders_cab);
+int elevator_init();
+
+
+// /**
+//  * @brief Set the cab button lights in accordance with the values in @p p_orders_cab
+//  * 
+//  * @param[in, out] p_orders_cab      A pointer to an array of the cab-button states
+//  */
+// void set_cab_button_lights(int* p_orders_cab);
 
 
 /**
@@ -49,15 +59,15 @@ int get_current_floor();
 void update_cab_buttons(int* p_orders_cab);
 
 
-/**
- * @brief Set the floor button lights in accordance with the values @p p_orders_up and @p p_orders_down .
- * 
- * @param[in, out] p_orders_up      A pointer to an array of the up-button states
- * @param[in, out] p_orders_down    A pointer to an array of the down-button states
- * 
- * Sets all floor button lights on or off, 
- */
-void set_floor_button_lights(int* p_orders_up, int* p_orders_down);
+// /**
+//  * @brief Set the floor button lights in accordance with the values @p p_orders_up and @p p_orders_down .
+//  * 
+//  * @param[in, out] p_orders_up      A pointer to an array of the up-button states
+//  * @param[in, out] p_orders_down    A pointer to an array of the down-button states
+//  * 
+//  * Sets all floor button lights on or off, 
+//  */
+// void set_floor_button_lights(int* p_orders_up, int* p_orders_down);
 
 
 /**
@@ -77,17 +87,4 @@ void set_floor_button_lights(int* p_orders_up, int* p_orders_down);
  */
 void update_floor_buttons(int* p_orders_up, int* p_orders_down);
 
-
-/**
- * @brief Initialize the elevator
- * 
- * @return A true value on successful initialization, 0 if it failed.
- * 
- * Initiailize the elevator by setting all connected values to its default values, and, 
- * if the elevator isn't already at a floor, drive the elevator down to the first valid floor.
- */
-int init_elevator();
-
-
 #endif //ELEVATOR_IO_H
-
