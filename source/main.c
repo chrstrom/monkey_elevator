0640e5e7e984bf19b1014ad6cf6097b2f5846ac2
Christopher <detoxte@gmail.com>
2020-02-22 22:17:01 +0100

    Minor renamings and reorganization
    Signed-off-by: Christopher <detoxte@gmail.com>

--- source/main.c
+++ source/main.c
@@ -21,29 +21,27 @@ int main(){
         exit(1);
     }
     
-    error = init_elevator();
+    error = elevator_init();
     if(error != 0){
         fprintf(stderr, "Unable to initialize software\n");
         exit(1);
     }
 
-    elevator_data_t elevator_data = {.door_open = DOOR_CLOSE, .last_floor = get_current_floor(), .last_dir = HARDWARE_MOVEMENT_STOP, .state = STATE_IDLE, .next_action = ACTION_STOP_MOVEMENT};
+    elevator_data_t elevator_data = {.door_open = DOOR_CLOSE,
+                                     .last_floor = get_current_floor(),
+                                     .last_dir = HARDWARE_MOVEMENT_STOP,
+                                     .state = STATE_IDLE,
+                                     .next_action = ACTION_STOP_MOVEMENT};
 
-    // ELEVATOR PROGRAM LOOP
     while (1){
         // Temporary
         elevator_data.last_floor = (get_current_floor() == BETWEEN_FLOORS ? elevator_data.last_floor : get_current_floor());
 
-        // Set floor light
         set_floor_indicator_light(get_current_floor());
 
-        // Handle button press events
         update_button_state(&elevator_data);
 
-        // Determine next action
         elevator_data.next_action = elevator_update_state(&elevator_data);
-
-        //Execute next action
         elevator_execute_next_action(&elevator_data);
     }
 }
